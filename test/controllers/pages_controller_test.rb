2cc55eba4335ef613ccca52e54fd50bfe1c804be
SneyderHOL <1507@holbertonschool.com>
2021-03-08 09:56:11 -0500

    Add an integration test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # test "should get home page" do
+  #   get root_url
+  #   assert_response :success
+  # end
+
+  # test "should get about page" do
+  #   get about_url
+  #   assert_response :success
   # end
 end
